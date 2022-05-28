b0ae2fa4f6fa660a74672d9560e1345e7325bad8
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-05-29 01:17:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("running fruits vendor server");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ojwr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const toolscollection = client.db("toolcomposer").collection("tools");
+
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolscollection.find(query);
+      const tools = await cursor.toArray();
+
+      res.send(tools);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("Listening to port " + port);
+});
